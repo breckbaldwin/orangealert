2664a8017f57b433456b5b6f60dc3cac1362ff94
Breck <breck@Karen-New-Mac.local>
2024-01-23 21:01:54 -0500

    initial

---
+++ streamlit/pages/07_Free_Will_Simulator.py
@@ -0,0 +1,53 @@
+import streamlit as st
+
+UI = st
+SS = st.session_state
+
+UI.title("Free Will Simulator")
+
+UI.markdown("Notes on what it would mean to have a free will in a simulated environment")
+
+UI.markdown("""
+- We have a very strong intuition that humans face choice and that factors like morality, goodness, empathy can usefully contribute to human behavior.
+            
+- The problem is that no one knows how to build such a system. What it comes down to is a combination of no function being uncomputable so far by a Turing machine, no adequate theory of concisiousness that breaks free of mechanistic determinism or the observation that systems can be deterministic without being predictable. 
+            
+- This page is an effort to explore how free will could be simulated without any regard to being true. 
+            """)
+
+UI.markdown("""
+### The Free Will Transistor (FWT)
+            
+The FWT stands for a decsion element in an otherwise deterministic system that controls the execution flow. It defers to some external source process that is essentially an 'if then' statement in a programming language that waits on some external event to allow the execution to continue in a direction. Somehow, free will gets to influence the FWT--it is the 'will' part of the proposition. 
+            
+- The FWT is a very unsatisfactory element in level 0 free will sim. It just defers the determinism to another level. 
+            
+- Where I see promise is in the FWT that manages to operate at the quantum uncertainty level, becuase we are looking like a big billiard ball situation up to that point. 
+            
+- It is not actually hard to bring uncertainly into the FWT since it can be influenced by non-deterministic events and therefore trivially breaks Turing computability since the same inputs will create differing outputs so we don't have a function anymore. But randomly driven FWTs are unsatisfactory in that we have replaced determinism with absence of intent because the FWT is random. The baby has been thrown out with the bath water... We will call such random decision processes random transistors (RT) and see if that helps. 
+            
+- Consider the first time you make a free will worthy decision. 
+`If I swipe this jelly bean I will enjoy eating it and annoy my friend who owns this jelly bean
+else I don't swipe this jelly bean which does not annoy my friend but I don't enjoy eating it` 
+            
+- Apply a RT, coin flip, and accumulate the consequences. 
+    1. Swipe + pleasure + annoy        
+    2. Don't swip + no pleasure + not annoy
+            
+- Either case then could become the foundation of a determinsitic elaboration that may have RT's or not. Lets assume we want the pleasure of eating the jelly bean and not annoying our friend. One could decide to steal the jelly bean, decieving our friend and not suffer the consequences of an annoyed friend or figure a different way to get a jelly bean that does not annoy a friend. 
+            
+- Since we have memory then we can learn that either 1. or 2. are survivable and we adopt them as future life strategies and the result of that event has set the overall moral direction of the person involved as further instances of similar circumstances pile in and there is now a tendancy towards 1. style response from the deterministic side or prediliction vs the 2. style response. That first RT set the cascade of decisions in that direction. Perhaps it takes many such RT's on the side or external factors reinforce the utility of 1. vs 2. and the brain learns. But for our purposes the since RT decision is enough to set the moral tone with RT applying probabilistically from then on. 
+            
+- But we have replaced the FWT with an experince reinforced RT. While unsatisfactory, there is at least a decision mechanism that resists determinism--the person however doesn't get much credit for the outcomes. Not very satisfactory but it feels like a not unreasonable fit if not failing to account for introspections of accoutability. 
+            
+- With this stochastic element we can simulate human morality pretty well and account for behavior. Human laws and conventions make sense as guardrails and priors on expected outcomes an nobody gets a pass because society has to evolve and society's rules matter. 
+
+- As for free will and the FWT it doesn't have a role but the appearance and assumption of free will does since it is how we adjust priors on decisions and consequences as society evolves. It is a booring but reasonable explanation for the phenomenon.  
+            
+- There are RT based approaches 
+    
+            
+
+            
+            
+            """)
