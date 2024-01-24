23f05ac34e60ddb100f3eea123589df5f6c001be
Breck <breck@Karen-New-Mac.local>
2024-01-23 21:00:45 -0500

    initial

---
+++ streamlit/pages/06_Return_to_Office_v_Remote_Work.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+UI = st
+SS = st.session_state
+
+UI.title("Return to Office (RTO) vs Work from Home (WFH): Simulating an Thorny Issue")
+
+UI.markdown("""
+The pandemic shutdown broke a foundational pillar of most work: you have to go into the office. Gone in a snap by executive fiat and yet work still got done, maybe more, innovation happened, maybe less and work life did not melt down into anarchy. All in all a remarkable event that would change work forever except some employers want it the way it was. 
+            
+Asserting RTO comes with very vauge justifications and little in the way of hard evidence that this is needed for the bottom line. Generally the lame excuse is that "water cooler conversations" are important for X. Counter arguments are that execs are trying to recover sunk costs in offices, managers feel useless unless their minions are before them for physical inspection and so on. 
+
+Lets pretend there is a rational case for RTO and simulate how things are different in the WFH environment. 
+            """)
